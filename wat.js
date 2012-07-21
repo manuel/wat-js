4e6934397c37d35a18cb628d10170fa9f54fed82
Manuel Simoni <msimoni@gmail.com>
2012-07-21 20:54:11 +0200

    some bug fixes

--- wat.js
+++ wat.js
@@ -1,11 +1,13 @@
 var wat = (function() {
     function KDone() {}
     function KApp(next, opd) { this.next = next; this.opd = opd }
+    function KDef(next, name) { this.next = next; this.name = name }
     function KEval1(next, eexpr) { this.next = next; this.eexpr = eexpr }
     function KEval2(next, expr) { this.next = next; this.expr = expr }
     function enter(k, e, val) { return k.enter(k, e, val) }
     KDone.prototype.enter = function(k, e, val) { return val }
     KApp.prototype.enter = function(k, e, opr) { return function() { return operate(opr, k.opd, k.next, e) } }
+    KDef.prototype.enter = function(k, e, val) { return function() { bind(e, k.name, val); return enter(k.next, VOID) } }
     KEval1.prototype.enter = function(k, e, expr) { return function() { return perform(k.eexpr, new KEval2(k.next, expr), e) } }
     KEval2.prototype.enter = function(k, e, newe) { return function() { return perform(k.expr, k.next, newe) } }
     function Lit(val) { this.val = val }
@@ -17,8 +19,8 @@ var wat = (function() {
     Xons.prototype.perform = function(obj, k, e) { return perform(car(obj), new KApp(k, obj), e) }
     function operate(opr, opd, k, e) { return opr.operate(opd, k, e) }
     Fun.prototype.operate = function(opd, k, e) {
-	xe = extend(e); bind(xe, fun.formal, opd); bind(xe, fun.eformal, e); return perform(fun.body, k, xe) }
-    Def.prototype.operate = function(opd, k, e) { bind(e, elt(opd, 1), elt(opd, 2)); return enter(k, VOID) }
+	var xe = extend(e); bind(xe, fun.formal, opd); bind(xe, fun.eformal, e); return perform(fun.body, k, xe) }
+    Def.prototype.operate = function(opd, k, e) { return perform(elt(opd, 2), new KDef(k, elt(opd, 1))) }
     CCC.prototype.operate = function(opd, k, e) { return perform(elt(opd, 1), new KApp(k, k), e) }
     Vau.prototype.operate = function(opd, k, e) { return enter(k, new Fun(elt(opd, 1), elt(opd, 2), elt(opd, 3))) }
     Eval.prototype.operate = function(opd, k, e) { return perform(elt(opd, 1), new KEval1(k, elt(opd, 2)), e) }
